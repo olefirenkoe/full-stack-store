54bc29dad900d6eea5652409b5036932040ddad5
olefirenkoe <olefirenkoevgen@gmail.com>
2021-03-12 22:04:11 +0200

    add routes

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const sequelize = require('./db')
 const models = require('./models/models')
 const cors = require('cors')
+const router = require('./routes/index')
 
 
 const PORT = process.env.PORT || 5000
@@ -10,11 +11,7 @@ const PORT = process.env.PORT || 5000
 const app = express()
 app.use(cors())
 app.use(express.json())
-
-app.get('/', (req, res) => {
-    res.status(200).json({message: 'WORKING!!!'})
-})
-
+app.use('/api', router)
 const start = async () => {
     try {
         await sequelize.authenticate()
