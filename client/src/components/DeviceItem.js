e40e265b82253ff1b3e590e53a85b30b68d2cf86
olefirenkoe <olefirenkoevgen@gmail.com>
2021-03-16 23:14:17 +0200

    add component for one device page

--- client/src/components/DeviceItem.js
+++ client/src/components/DeviceItem.js
@@ -1,22 +1,26 @@
 import React from 'react';
 import {Card, Col, Image} from "react-bootstrap";
-import star from '../assets/star.png'
+import star from '../assets/star.png';
+import {useHistory} from 'react-router-dom';
+import {DEVICE_ROUTE} from '../utils/consts';
 
 const DeviceItem = ({device}) => {
+    const history = useHistory()
     return (
-        <Col md={3}>
+        <Col md={3} className="mt-3" onClick={() => history.push(DEVICE_ROUTE + '/' + device.id)}>
             <Card
                 style={{cursor: 'pointer', width: 150}}
                 border={'light'}
             >
                 <Image width={150} height={150} src={device.img}/>
-                <div>
-                    <div>{device.name}</div>
-                    <div>
+                <div className="mt-1 text-black-50 d-flex justify-content-between align-items-center">
+                    <div>Brand...</div>
+                    <div className="d-flex align-items-center">
                         <div>{device.rating}</div>
-                        <Image src={star}/>
+                        <Image src={star} width={15} height={15}/>
                     </div>
                 </div>
+                <div>{device.name}</div>
             </Card>
         </Col>
     );
--- client/src/pages/DevicePage.js
+++ client/src/pages/DevicePage.js
@@ -1,10 +1,53 @@
 import React from 'react';
+import {Button, Card, Col, Container, Image, Row} from "react-bootstrap";
+import bigStar from '../assets/bigStar.png'
 
 const DevicePage = () => {
+    const device =  {id: 1, name: "Iphone 12 pro", price: 25000, rating:5, img:'https://cdn.dxomark.com/wp-content/uploads/medias/post-61183/iphone-12-pro-blue-hero.jpg'};
+    const description = [
+        {id: 1, title: 'RAM', description: '5GB'},
+        {id: 2, title: 'Camera', description: '12mp'},
+        {id: 3, title: 'CPU', description: 'SnapDragon'},
+        {id: 4, title: 'AMR', description: '5GB'},
+        {id: 5, title: 'Accumulator', description: '4000 MAh'},
+
+    ]
     return (
-        <div>
-          DEVICEPAGE
-        </div>
+        <Container className="mt-3">
+            <Row>
+                <Col md={4}>
+                    <Image width={300} height={300} src={device.img}/>
+                </Col>
+                <Col md={4}>
+                    <Row className="d-flex flex-column align-items-center">
+                        <h1>{device.name}</h1>
+                        <div
+                            className="d-flex align-items-center justify-content-center"
+                            style={{background: `url(${bigStar}) no-repeat center center`, width: 240, height: 240, backgroundSize: 'cover', fontSize: 64}}
+                        >
+                            {device.rating}
+                        </div>
+                    </Row>
+                </Col>
+                <Col md={4}>
+                    <Card
+                        className="d-flex flex-column align-items-center justify-content-around"
+                        style={{width: 300, height: 300, fontsize: 32, border: '5px solid lightgrey'}}
+                    >
+                        <span>{device.price} UAH</span>
+                        <Button variant={"outline-danger"}>Add to cart</Button>
+                    </Card>
+                </Col>
+            </Row>
+            <Row className="d-flex flex-column m-3">
+                <h2>Description</h2>
+                {description.map((info, index) =>
+                    <Row key={info.id} style={{background: index % 2 === 0 ? 'lightgray' : 'transparent', padding: 10}}>
+                        {info.title}: {info.description}
+                    </Row>
+                )}
+            </Row>
+        </Container>
     );
 };
 
