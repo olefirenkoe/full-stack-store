441e8cbe71974ad474f1fd86a0c9f801d35d7d00
olefirenkoe <olefirenkoevgen@gmail.com>
2021-03-17 19:34:08 +0200

    made admin panel

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -2,19 +2,33 @@ import React, {useContext} from 'react';
 import {Context} from "../index";
 import {Button, Container, Nav, Navbar} from "react-bootstrap";
 import {NavLink} from "react-router-dom";
-import {SHOP_ROUTE} from "../utils/consts";
+import {ADMIN_ROUTE, SHOP_ROUTE, LOGIN_ROUTE} from "../utils/consts";
 import {observer} from "mobx-react-lite";
+import {useHistory} from 'react-router-dom';
 
 const NavBar = observer( () => {
-    const {user} = useContext(Context)
+    const {user} = useContext(Context);
+    const history = useHistory();
+
     return (
         <Navbar bg="dark" variant="dark">
            <Container>
                <NavLink style={{color:'white'}} to={SHOP_ROUTE}>VILKA-shop</NavLink>
                {user.isAuth ?
                    <Nav className="ml-auto">
-                       <Button variant={"outline-light"}>Admin</Button>
-                       <Button variant={"outline-light"} className="ml-2">Log out</Button>
+                       <Button
+                           variant={"outline-light"}
+                           onClick={() => history.push(ADMIN_ROUTE)}
+                       >
+                           Admin
+                       </Button>
+                       <Button
+                           variant={"outline-light"}
+                           onClick={() => history.push(LOGIN_ROUTE)}
+                           className="ml-2"
+                       >
+                           Log out
+                       </Button>
                    </Nav>
                    :
                    <Nav className="ml-auto">
