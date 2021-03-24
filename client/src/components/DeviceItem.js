8c49d47ac47e791bcbdb62362e99baaf83221ab5
olefirenkoe <olefirenkoevgen@gmail.com>
2021-03-24 11:45:26 +0200

    modified deviceItem component

--- client/src/components/DeviceItem.js
+++ client/src/components/DeviceItem.js
@@ -16,10 +16,10 @@ const DeviceItem = ({device}) => {
             >
                 <Image width={150} height={150} src={process.env.REACT_APP_API_URL + device.img}/>
                 <div className="mt-1 text-black-50 d-flex justify-content-between align-items-center">
-                    <div>{device.typeId}</div>
+                    <div>{device.price} UAH</div>
                     <div className="d-flex align-items-center">
                         <div>{device.rating}</div>
-                        <Image src={star} width={15} height={15}/>
+                        <Image src={star} width={15} height={15} className={"ml-1"}/>
                     </div>
                 </div>
                 <div>{device.name}</div>
